2553b074aa1894c3699230a5d0d5ca1f7a8beaff
shamoji-tech <suguru_toyohara@outlook.jp>
2020-05-30 21:20:37 +0900

    リファクタリング

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import slackweb
 import os
 import base64
-def main(context,context2):
-    print(context,"1_type:",type(context),context["data"])
-    slack=slackweb.Slack(url=os.environ["SLACK_WEBHOOK_URL"])
-    bs64code=context["data"]
-    send_text=base64.b64decode(bs64code).decode()
-    slack.notify(text=send_text)
-    print(send_text)
+from dao.dao import SlackMessager,ExtractMessageFromPubSubContext,OSEnvironmentState
+
+def send_message(context, info):
     
-
-
+    webhook=OSEnvironmentState.getSlackWebhook()
+    slackMessager=SlackMessager(webhook=webhook)
+    message = ExtractMessageFromPubSubContext.messageDecode(context)
+    
+    slackMessager.send(message)
+    
+    print(context, info) #for logging
