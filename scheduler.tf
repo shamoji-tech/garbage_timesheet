fcfcdfdc210b461298bd6a8f6800def310c2598d
shamoji-tech <suguru_toyohara@outlook.jp>
2020-05-30 17:54:59 +0900

    水曜日版を作成

--- scheduler.tf
+++ scheduler.tf
@@ -3,12 +3,26 @@ resource "google_cloud_scheduler_job" "slack-notify-scheduler-burnable" {
     name    = "slack-notify-burnable"
     project = var.project
     description = "燃えるゴミの日通知"
-    schedule = "00 21 * * 0,3"
+    schedule = "00 21 * * 0"
     time_zone = "Asia/Tokyo"
 
     pubsub_target {
         topic_name = google_pubsub_topic.slack_notify.id
-        data       = base64encode("日曜日の9時になりました。明日は燃えるゴミの日ですので、燃えるゴミを門の外に出しましょう。")
+        data       = base64encode("日曜日の9時になりました。明日は月曜日で燃えるゴミの日ですので、燃えるゴミを門の外に出しましょう。")
+    }
+}
+
+resource "google_cloud_scheduler_job" "slack-notify-scheduler-burnable-2" {
+
+    name    = "slack-notify-burnable-2"
+    project = var.project
+    description = "燃えるゴミの日通知(水曜版)"
+    schedule = "00 21 * * 3"
+    time_zone = "Asia/Tokyo"
+
+    pubsub_target {
+        topic_name = google_pubsub_topic.slack_notify.id
+        data       = base64encode("水曜日の9時になりました。明日は木曜日で燃えるゴミの日ですので、燃えるゴミを門の外に出しましょう。")
     }
 }
 
@@ -22,7 +36,7 @@ resource "google_cloud_scheduler_job" "slack-notify-scheduler-resource" {
 
     pubsub_target {
         topic_name = google_pubsub_topic.slack_notify.id
-        data       = base64encode("月曜日の9時になりました。明日は資源ゴミの日です。段ボールとペットボトル、空き缶などを門のそとに出しましょう。")
+        data       = base64encode("月曜日の9時になりました。明日は火曜日で資源ゴミの日です。段ボールとペットボトル、空き缶などを門のそとに出しましょう。")
     }
 }
 
